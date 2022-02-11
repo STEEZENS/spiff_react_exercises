546f31b74bda68fa1c03ef7837bd491590269888
Cam Stephens <steezens@gmail.com>
2022-02-11 15:24:04 -0700

    add breakpoints toggle

--- src/solutions/ProgressBarSolution/index.js
+++ src/solutions/ProgressBarSolution/index.js
@@ -1,71 +1,12 @@
 import React, { useState, useReducer } from 'react';
-import Button from '../../components/Button';
 import ProgressBar from '../../components/ProgressBar';
+import Button from '../../components/Button';
+import InputCheckbox from '../../components/InputCheckbox';
 
 const INACTIVE = 'INACTIVE';
 const LOADING = 'LOADING';
 const FINISHED = 'FINISHED';
 const HANG_AT_PERCENT = 90;
-
-function animationMapReducer(state, {
-  type,
-  percent,
-  transitionDuration,
-  transitionTimingFunction,
-}) {
-  switch (type) {
-    case INACTIVE:
-      return {
-        width: 0,
-        isLoading: false,
-        isFinished: false,
-        transitionDuration: 0,
-      };
-
-    case LOADING:
-      return {
-        width: percent || HANG_AT_PERCENT,
-        isLoading: true,
-        isFinished: false,
-        transitionDuration,
-        transitionTimingFunction,
-      };
-
-    case FINISHED:
-      return {
-        width: 100,
-        isLoading: true,
-        isFinished: true,
-        transitionDuration: 1000,
-        transitionTimingFunction: 'ease',
-      };
-
-    default:
-      return state;
-  }
-};
-
-function normalizeBreakpoints(breakpoints, hangAtMs) {
-  const breakpointsFiltered = [...breakpoints]
-    .filter((b) => b < HANG_AT_PERCENT);
-
-  const breakpointsSorted = [...breakpointsFiltered, HANG_AT_PERCENT]
-    .sort((a, b) => a - b);
-
-  const breakpointsDecorated = breakpointsSorted.map((breakpoint, index) => {
-    const prevB = breakpointsSorted[index - 1] || 0;
-    const duration = hangAtMs * ((breakpoint - prevB) / 100);
-
-    return {
-      width: breakpoint,
-      transitionDuration: duration,
-      transitionTimingFunction: 'ease',
-    }
-  });
-
-  return breakpointsDecorated;
-}
-
 
 const ProgressBarSolution = ({
   breakpoints = [12, 54, 72],
@@ -85,6 +26,70 @@ const ProgressBarSolution = ({
     isFinished: false,
     transitionDuration: 0,
   });
+
+  const [
+    isBreakpoints,
+    setIsBreakpoints,
+  ] = useState(true);
+
+  function animationMapReducer(state, {
+    type,
+    percent,
+    transitionDuration,
+    transitionTimingFunction,
+  }) {
+    switch (type) {
+      case INACTIVE:
+        return {
+          width: 0,
+          isLoading: false,
+          isFinished: false,
+          transitionDuration: 0,
+        };
+
+      case LOADING:
+        return {
+          width: percent || HANG_AT_PERCENT,
+          isLoading: true,
+          isFinished: false,
+          transitionDuration: transitionDuration || hangAtMs,
+          transitionTimingFunction: transitionTimingFunction || 'linear',
+        };
+
+      case FINISHED:
+        return {
+          width: 100,
+          isLoading: true,
+          isFinished: true,
+          transitionDuration: 1000,
+          transitionTimingFunction: 'ease',
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  function normalizeBreakpoints(breakpoints, hangAtMs) {
+    const breakpointsFiltered = [...breakpoints]
+      .filter((b) => b < HANG_AT_PERCENT);
+
+    const breakpointsSorted = [...breakpointsFiltered, HANG_AT_PERCENT]
+      .sort((a, b) => a - b);
+
+    const breakpointsDecorated = breakpointsSorted.map((breakpoint, index) => {
+      const prevB = breakpointsSorted[index - 1] || 0;
+      const duration = hangAtMs * ((breakpoint - prevB) / 100);
+
+      return {
+        width: breakpoint,
+        transitionDuration: duration,
+        transitionTimingFunction: 'ease',
+      }
+    });
+
+    return breakpointsDecorated;
+  }
 
   function recurseBreakpoints(breakpoints, activeIndex) {
     const {
@@ -112,18 +117,30 @@ const ProgressBarSolution = ({
   }
 
   function startRequest() {
-    recurseBreakpoints(normalizeBreakpoints(breakpoints, hangAtMs), 0);
+    return isBreakpoints
+      ? recurseBreakpoints(normalizeBreakpoints(breakpoints, hangAtMs), 0)
+      : dispatchAnimation({ type: LOADING });
   }
 
   function finishRequest() {
-    clearTimeout(breakpointTimeoutId);
-    setBreakpointTimeoutId(null);
+    clearBreakpointTimeoutId();
     dispatchAnimation({ type: FINISHED });
     setTimeout(() => resetRequest(), 3000);
   }
 
   function resetRequest() {
     dispatchAnimation({ type: INACTIVE });
+  }
+
+  function clearBreakpointTimeoutId() {
+    clearTimeout(breakpointTimeoutId);
+    setBreakpointTimeoutId(null);
+  }
+
+  function handleProgressTypeInput() {
+    clearBreakpointTimeoutId();
+    resetRequest();
+    setIsBreakpoints(!isBreakpoints);
   }
 
   return (
@@ -137,28 +154,40 @@ const ProgressBarSolution = ({
         transitionTimingFunction={animationState.transitionTimingFunction}
       />
 
-      <Button
-        className="Button-start-request"
-        theme="green"
-        isDisabled={animationState.isLoading}
-        isLoading={animationState.isLoading}
-        onClick={startRequest}>
-        <span
-          className="__content">
-          Start Request
-        </span>
-        <span
-          className="__loading">
-          Loading...
-        </span>
-      </Button>
+      <div
+        className="ProgressBarSolution__actions">
+        <InputCheckbox
+          isChecked={isBreakpoints}
+          isDisabled={animationState.isFinished}
+          onInput={handleProgressTypeInput}>
+          Load with breakpoints
+        </InputCheckbox>
 
-      <Button
-        theme="red"
-        isDisabled={animationState.isFinished || !animationState.isLoading}
-        onClick={finishRequest}>
-        Finish Request
-      </Button>
+        <div>
+          <Button
+            className="Button-start-request"
+            theme="green"
+            isDisabled={animationState.isLoading}
+            isLoading={animationState.isLoading}
+            onClick={startRequest}>
+            <span
+              className="__content">
+              Start Request
+            </span>
+            <span
+              className="__loading">
+              Loading...
+            </span>
+          </Button>
+
+          <Button
+            theme="red"
+            isDisabled={animationState.isFinished || !animationState.isLoading}
+            onClick={finishRequest}>
+            Finish Request
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
